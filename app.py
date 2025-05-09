32e00558cc3a1199be1341a0d672cd83aa162254
RyanRajDev <82542223+0RenDev@users.noreply.github.com>
2025-05-08 21:42:20 -0700

    output added

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import requests, json
+from nicegui import ui
 
+ui.label('Hello NiceGUI!')
+ui.button('BUTTON', on_click=lambda: ui.notify('button was pressed'))
+
+ui.run()
 
 api_key = "J79N6BMQG2YDZ7EUGA9QSNKH3"
 
 base_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
 
-# city = input("Enter city name (Limit to within the US): ")
+city = input("Enter city name (Limit to within the US): ")
 
-city = "Pleasanton"
+# city = "Pleasanton"
 
 complete_url = base_url + city + "?unitGroup=us&key="+ api_key + "&contentType=json"
 
@@ -17,10 +22,17 @@ response = requests.get(complete_url)
 res_json = response.json()
 
 
-json_obj = json.dumps(res_json, indent=4)
-
-print(res_json["currentConditions"]["temp"])
 
 
-with open("temp.json", "w") as outfile:
-    outfile.write(json_obj)
+
+print("The current weather in " + city + " is: " + str(res_json["currentConditions"]["temp"]) + "F")
+
+
+
+
+def ret_currenttemp(response):
+    return "The current weather in " + city + " is: " + str(res_json["currentConditions"]["temp"]) + "F"
+
+
+ret_currenttemp(res_json)
+
